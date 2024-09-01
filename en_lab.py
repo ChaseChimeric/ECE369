dda3d80a9a8d6328eed9b0424b928ce84aa87bb8
ChaseChimeric <ryfong704@gmail.com>
2024-08-31 22:42:20 -0700

    enscriptified gitignore

---
+++ en_lab.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import os
+import sys
+import re
+it = sys.argv[1]
+
+output = ""
+
+with open(".gitignore", "r") as inputFile:
+    inputText = inputFile.read().split("\n")
+    for s in inputText:
+        
+        if it != re.sub(r"\D", "", s):
+
+            pass
+        else:
+            if '*' in s:
+                s = s[:-2]
+            else:
+                s += "**"
+        output += s + '\n'
+with open(".gitignore", "w") as outputfile:
+    outputfile.write(output)
+
