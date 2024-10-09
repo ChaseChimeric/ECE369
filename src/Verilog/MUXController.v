61181e68e6bee42072f2ae87fe8130037e417f99
Ahmad Qureshi <ahmadsqureshi@arizona.edu>
2024-10-09 10:58:23 -0700

    Progress

--- src/Verilog/MUXController.v
+++ src/Verilog/MUXController.v
@@ -52,6 +52,42 @@ module MUXController (
                 AdderAdd <= 0;
                 sh_amt <= ((op == 6'000010) | (op == 6'000000))
             end
+
+            // J
+            6'b000010: begin
+                ForceInstr <= 0;
+                ZeroInverted <= X;
+                Inverted <= X;
+                Sum <= 0;
+                NextInstr <= 0;
+                NextInstrAddress <= X;
+                DataWriteVal <= X;
+                inA <= X;
+                ALUImmReg <= X;
+                WB_RA <= X;
+                WBDest <= X;
+                Sign <= X;
+                AdderAdd <= 1;
+                sh_amt <= X;
+            end
+
+            // JR
+            6'b000010: begin
+                ForceInstr <= 1;
+                ZeroInverted <= X;
+                Inverted <= 0;
+                Sum <= 0;
+                NextInstr <= 1;
+                NextInstrAddress <= X;
+                DataWriteVal <= X;
+                inA <= 1;
+                ALUImmReg <= X;
+                WB_RA <= X;
+                WBDest <= X;
+                Sign <= 0;
+                AdderAdd <= 1;
+                sh_amt <= X;
+            end
             
             // Addi Instruction
             6'b001000 begin
@@ -139,22 +175,114 @@ module MUXController (
                 sh_amt <= 0;
             end
 
-            // slt
-            6'b000000: begin
+            // Load Types
+            // Load Word
+            6'b100011: begin
                 ForceInstr <= X;
                 ZeroInverted <= X;
-                Inverted <= (op == 6'b100111);
-                Sum <= 0;
+                Inverted <= 0;
+                Sum <= 0
+                NextInstr <= 0;
+                NextInstrAddress <= 0;
+                DataWriteVal <= 0;
+                inA <= 0;
+                ALUImmReg <= 1; 
+                WB_RA <= 0;
+                WBDest <= 1;
+                Sign <= 0;
+                AdderAdd <= 0;
+                sh_amt <= 0;
+            end
+
+            // LH
+            6'b100001: begin
+                ForceInstr <= X;
+                ZeroInverted <= X;
+                Inverted <= 0;
+                Sum <= 0
+                NextInstr <= 0;
+                NextInstrAddress <= 0;
+                DataWriteVal <= 0;
+                inA <= 0;
+                ALUImmReg <= 1; 
+                WB_RA <= 0;
+                WBDest <= 1;
+                Sign <= 0;
+                AdderAdd <= 0;
+                sh_amt <= 0;
+            end
+
+            // LB
+            6'b100000: begin
+                ForceInstr <= X;
+                ZeroInverted <= X;
+                Inverted <= 0;
+                Sum <= 0
+                NextInstr <= 0;
+                NextInstrAddress <= 0;
+                DataWriteVal <= 0;
+                inA <= 0;
+                ALUImmReg <= 1; 
+                WB_RA <= 0;
+                WBDest <= 1;
+                Sign <= 0;
+                AdderAdd <= 0;
+                sh_amt <= 0;
+            end
+
+            // Store Type
+            // Store Word
+            6'b101011 begin
+                ForceInstr <= X;
+                ZeroInverted <= X;
+                Inverted <= 0;
+                Sum <= 0
                 NextInstr <= 0;
                 NextInstrAddress <= 0;
                 DataWriteVal <= 1;
                 inA <= 0;
-                ALUImmReg <= 0;
-                WB_RA <= 0;
-                WBDest <= 0;
+                ALUImmReg <= 1; 
+                WB_RA <= X;
+                WBDest <= X;
                 Sign <= 0;
-                AdderAdd <= 0;
-                sh_amt <= ((op == 6'000010) | (op == 6'000000))
+                AdderAdd <= 1;
+                sh_amt <= 0;
+            end
+
+            // Store HalfWord
+            6'b101001 begin
+                ForceInstr <= X;
+                ZeroInverted <= X;
+                Inverted <= 0;
+                Sum <= 0
+                NextInstr <= 0;
+                NextInstrAddress <= 0;
+                DataWriteVal <= 1;
+                inA <= 0;
+                ALUImmReg <= 1; 
+                WB_RA <= X;
+                WBDest <= X;
+                Sign <= 0;
+                AdderAdd <= 1;
+                sh_amt <= 0;
+            end
+
+            // Store Byte
+            6'b101000 begin
+                ForceInstr <= X;
+                ZeroInverted <= X;
+                Inverted <= 0;
+                Sum <= 0
+                NextInstr <= 0;
+                NextInstrAddress <= 0;
+                DataWriteVal <= 1;
+                inA <= 0;
+                ALUImmReg <= 1; 
+                WB_RA <= X;
+                WBDest <= X;
+                Sign <= 0;
+                AdderAdd <= 1;
+                sh_amt <= 0;
             end
 
             // Unsupported Instruction, treated like R-type
