b792fd3380b73855df3e416e0e1958db5f03473e
ChaseChimeric <ryfong704@gmail.com>
2024-10-11 10:23:49 -0700

    fix mux2 add mux4 cont. MemAcc

--- src/Verilog/MemoryAccess.v
+++ src/Verilog/MemoryAccess.v
@@ -2,15 +2,15 @@ module MemoryAccess(
     input Clk,
     input Sum,
     input NextInstr,
-    input NextInstrAddress,
+    //input NextInstrAddress,
     input DataWriteVal,
     input wb_ra,
     input MemMode [1:0],
     input ReadAddr [31:0], //from ALUOut
     input ZeroOrNot,
-    input ForceInstr,
+    input ForceInstr [1:0],
     input rt_dat [31:0], // instruction 20:16 DATA AT INStrUCTION
-    input immExtended [15:0], // instruction 16:0
+    input immExtended [15:0], // instruction 15:0
     // alu again but to output
     input NextInstrAddress [31:0],
     input FullInstruction [31:0],
@@ -30,13 +30,16 @@ module MemoryAccess(
     output reg MemoryRead_out [31:0], //from ALUOut
     output reg immExtended_out [15:0], // instruction 16:0
     output reg ALU_out [31:0],
-    // alu again but to output
     output reg NextInstrAddress_out [31:0],
     output reg FullInstruction_out [31:0],
-
+    output reg WBDest_out,
     output reg AdderAdd_out,
     output reg RegWrite_out,
 );
+
+wire AdderAdd_sig;
+wire MemoryRead_sig;
+wire InstrAdd_sig;
 
 
 DataMemory datamemory(
@@ -45,11 +48,45 @@ DataMemory datamemory(
     .Clk(Clk), 
     .MemWrite(MemWriteEn), 
     .MemRead(MemReadEn), 
-    .ReadData(MemoryRead_out)
+    .ReadData(MemoryRead_sig) // add wire?
+);
+
+Mux32Bit4to1 mux4(
+    .in0(1'b0),
+    .in1(1'b1),
+    .in2(ZeroOrNot),
+    .in3(ReadAddr),
+    .sel(ForceInstr),
+    .mux_out(AdderAdd_sig)
+
+);
+
+Mux32Bit2To1 mux2(
+    .in0(FullInstruction),
+    .in1(ReadAddr),
+    .sel(AdderAdd_sig),
+    .mux_out(InstrAdd_sig)
 
 );
 
 
+
+always @(*) begin
+    Sum_out <= Sum;
+    NextInstr_out <= NextInstr;
+    NextInstrAddress_out <= NextInstrAddress;
+    DataWriteVal_out <= DataWriteVal;
+    wb_ra_out <= wb_ra;
+    InstrAdd_out <= //new mux
+    AdderAdd_out <= AdderAdd_sig;
+    MemoryRead_out <= MemoryRead_sig;
+    immExtended_out <= immExtended;
+    ALU_out <= ReadAddr;
+    NextInstrAddress_out <= NextInstrAddress;
+    FullInstruction_out <= FullInstruction;
+    WBDest_out <= WBDest;
+    RegWrite_out <= RegWrite;
+end
 
 
 endmodule
--- src/Verilog/Mux32Bit4to1.v
+++ src/Verilog/Mux32Bit4to1.v
@@ -7,13 +7,15 @@
 // Description - Performs signal multiplexing between 2 32-Bit words.
 ////////////////////////////////////////////////////////////////////////////////
 
-module Mux32Bit2To1 #(
+module Mux32Bit4To1 #(
     parameter SIZE = 32
 )
 (
     mux_out, 
     in0, 
-    in1, 
+    in1,
+    in2,
+    in3, 
     sel
 );
 
@@ -21,8 +23,17 @@ module Mux32Bit2To1 #(
     
     input [SIZE-1:0] in0;
     input [SIZE-1:0] in1;
-    input sel;
+    input [SIZE-1:0] in2;
+    input [SIZE-1:0] in3;
+    input sel [1:0];
 
-    assign mux_out = sel ? in0 : in1; 
+    always @(*) begin
+        case (sel)
+            2'b00: mux_out <= in0;
+            2'b01: mux_out <= in1;
+            2'b10: mux_out <= in2;
+            2'b11: mux_out <= in3;
+        endcase
+    end
 
 endmodule
