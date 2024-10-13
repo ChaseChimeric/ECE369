8e6bda465f4b1ce9d05dab2589c1aa731ae4c111
Ahmad Qureshi <ahmadsqureshi@arizona.edu>
2024-10-13 09:35:00 -0700

    Completed Execute

---
+++ src/Verilog/Execute.v
@@ -0,0 +1,96 @@
+module Execute (
+    input clk,
+    input ZeroInverted,
+    input Inverted,
+    input Sum,
+    input NextInstruction,
+    input NextInstrAddress,
+    input DataWriteVal,
+    input WB_RA,
+    input Sign,
+    input inA,
+    input ALUImmReg,
+    input WBDest,
+    input ForceInstr,
+    input MemReadEn,
+    input AdderAdd,
+    input MemWriteEn,
+    input sh_amt,
+    input RegWrite,
+    input [1:0] MemMode,
+    input [31:0] NextInstr,
+    input [31:0] Instr,
+    input [31:0] DataAtInstr25_21In,
+    input [31:0] DataAtInstr20_16In,
+    output reg SumOut,
+    output reg ZeroOrNot,
+    output reg NextInstructionOut,
+    output reg NextInstrAddressOut,
+    output reg DataWriteValOut,
+    output reg WB_RAOut,
+    output reg RegWriteOut,
+    output reg MemWriteEnOut,
+    output reg AdderAddOut,
+    output reg MemReadEnOut,
+    output reg ForceInstr,
+    output reg WBDestOut,
+    output reg [1:0] MemModeOut,
+    output reg [31:0] NextInstrOut,
+    output reg [31:0] InstrOut,
+    output reg [31:0] DataAtInstruction20_16,
+    output reg [31:0] ImmediateExtended,
+    output reg [31:0] ALUOut
+);
+
+    always @(posedge clk) begin
+        SumOut <= Sum;
+        NextInstructionOut <= NextInstruction;
+        NextInstrAddressOut <= NextInstrAddress;
+        DataWriteValOut <= DataWriteVal;
+        WB_RAOut <= WB_RA;
+        RegWriteOut <= RegWrite;
+        MemWriteEnOut <= MemWriteEn;
+        AdderAddOut <= AdderAdd;
+        MemReadEnOut <= MemReadEn;
+        ForceInstrOut <= ForceInstr;
+        WBDestOut <= WBDest;
+        MemModeOut <= MemMode;
+        NextInstrOut <= NextInstr;
+        InstrOut <= Instr;
+    end
+
+    wire [31:0] SignExtendedInstrImm;
+    wire [2:0] ALUOpCode;
+
+    SignExtension signExtender (
+        .sign_extend_in(Instr[15:0]),
+        .sign_extend_out(SignExtendedInstrImm)
+    );
+
+    wire [31:0] Mux1Out;
+    assign Mux1Out = (ALUImmReg) ? SignExtendedInstrImm : DataAtInstruction20_16In;
+
+    wire [31:0] Mux2Out;
+    assign Mux2Out = (inA) ? 32'd0 : Mux1Out;
+
+    wire [31:0] Mux3Out;
+    assign Mux3Out = (sh_amt) ? {27'd0, Instr[10:6]} : Mux2Out;
+
+    ALUController aluController (
+        .op(Instr[31:25]),
+        .funct(Instr[5:0]),
+        .opCode(ALUOpCode)
+    );
+
+    wire ZeroOutALU;
+    ALU32Bit ALU (
+        .ALUControl(ALUOpCode), 
+        .A(DataAtInstr25_21In), 
+        .B(DataAtInstr20_16In), 
+        .ALUResult(ALUOut), 
+        .Zero(ZeroOutALU)
+    );
+
+    assign ZeroOrNot = (ZeroInverted) ? (!ZeroOutALU) : (ZeroOutALU);
+    
+endmodule
