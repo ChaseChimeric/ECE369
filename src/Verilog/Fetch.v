c9b259f86570352812032f25c78ca9766abb6284
Ahmad Qureshi <ahmadsqureshi@arizona.edu>
2024-10-13 09:14:52 -0700

    Fetch started

---
+++ src/Verilog/Fetch.v
@@ -0,0 +1,55 @@
+module Fetch (
+    rst,
+    clk,
+    jumpAddress,
+    instrAdd,
+    imm,
+    adderAdd,
+    nextInstr,
+    sum,
+    nextInstr,
+    instrOut,
+    ALUOut
+);
+    input instrAdd, adderAdd, sum, rst, clk;
+    input [31:0] jumpAddress, imm, ALUOut;
+    output reg [31:0] instrOut;
+    output reg [31:0] nextInstr;
+
+
+    wire [31:0] PCAdderIn;
+    wire [31:0] PCAddResult;
+    wire [31:0] internalNextInstr;
+    wire [31:0] secondAdderOutput;
+    wire [31:0] programCounterAddress;
+    wire [31:0] instrMemAddress;
+    wire [31:0] instrOutInternal;
+
+    PCAdder add0(
+        .PCResult(PCAdderIn),
+        .PCAddResult(internalNextInstr)
+    );
+
+    ProgramCounter pc0(
+        .Address(PCAddResult),
+        .PCResult(instrMemAddress),
+        .Reset(rst),
+        .Clk(clk)
+    );
+
+    InstructionMemory mem0(
+        .Address(instrMemAddress),
+        .Instruction(instrOutInternal)
+    );
+
+    
+    assign PCAdderIn = (sum) ? (instrMemAddress + (jumpAddress)) : instrMemAddress;
+    assign secondAdderOutput = (adderAdd) ? (internalNextInstr + (imm << 2)) : (internalNextInstr);
+    assign addressOut = (nextInstr) ? (jumpAddress << 2) : secondAdderOutput;
+   
+    always @(posedge clk) begin
+        instrOut <= instrOutInternal;
+        nextInstr <= internalNextInstr;
+    end
+
+endmodule
