58578069452be02e0b62eb6f485060d953f73737
Ahmad Qureshi <ahmadsqureshi@arizona.edu>
2024-10-13 08:17:04 -0700

    Completed Decode, still to check synth

--- src/Verilog/Decode.v
+++ src/Verilog/Decode.v
@@ -21,12 +21,113 @@ module Decode (
     DataIn20_15,
     MemReadEn,
     MemWriteEn,
-    RegWrite
+    RegWrite,
+    sh_amt
 );
 
     input [31:0] InstructionIn, NextInstructionIn;
-    output [31:0] InstructionOut, NextInstructionOut;
+    output reg [31:0] InstructionOut, NextInstructionOut, DataIn25_21, DataIn20_15;
+    wire [31:0] DataIn25_21Wire, DataIn20_15Wire;
+
     input clk;
-    output 
+    output reg [2:0] ForceInstr;
+    wire [2:0] ForceInstrWire;
+    output reg ZeroInverted, 
+               Inverted, 
+               Sum, 
+               NextInstr, 
+               NextInstrAddress, 
+               DataWriteVal, 
+               ALUImmReg, 
+               WB_RA, 
+               WBDestRdRt, 
+               Sign, 
+               inA, 
+               AdderAdd, 
+               MemReadEn, 
+               MemWriteEn, 
+               RegWrite,
+               sh_amt;
+
+    wire ForceInstrWire,
+         ZeroInvertedWire,
+         InvertedWire, 
+         SumWire, 
+         NextInstrWire, 
+         NextInstrAddressWire, 
+         DataWriteValWire, 
+         ALUImmRegWire, 
+         WB_RAWire, 
+         WBDestRdRtWire, 
+         SignWire, 
+         inAWire, 
+         AdderAddWire, 
+         MemReadEnWire, 
+         MemWriteEnWire, 
+         RegWriteWire,
+         sh_amtWire,
+         RegReadWire;
     
+    always @(posedge clk) begin
+        InstructionOut <= InstructionIn;
+        NextInstructionOut <= NextInstructionIn;
+        ZeroInverted <= ZeroInvertedWire;
+        Inverted <= InvertedWire;
+        Sum <= SumWire;
+        NextInstrWire <= NextInstr;
+        NextInstrAddress <= NextInstrAddressWire;
+        DataWriteVal <- DataWriteVal;
+        ALUImmReg <= ALUImmRegWire;
+        WB_RA <= WB_RAWire;
+        WBDestRdRt <= WBDestRdRtWire;
+        Sign <= SignWire;
+        inA <= inAWire;
+        sh_amt <= sh_amtWire;
+        MemReadEn <= MemReadEnWire;
+        MemWriteEn <= MemReadEnWire;
+        RegWrite <= RegWriteWire;
+        ForceInstr <= ForceInstrWire;
+    end
+
+    MUXController mux_ctrl (
+        .funct(InstructionIn[31:26]),
+        .op(InstructionIn[5:0]),
+        .ForceInstr(ForceInstrWire),
+        .ZeroInverted(ZeroInvertedWire),
+        .Inverted(InvertedWire),
+        .Sum(SumWire),
+        .NextInstr(NextInstrWire),
+        .NextInstrAddress(NextInstrAddressWire),
+        .DataWriteVal(DataWriteValWire),
+        .inA(inAWire),
+        .ALUImmReg(ALUImmRegWire),
+        .WB_RA(WB_RAWire),
+        .WBDest(WBDestRdRtWire),
+        .Sign(SignWire),
+        .sh_amt(sh_amtWire),
+        .branchZeroVal(InstructionIn[20:16])
+    );
+
+    MemController mem_ctrl (
+        .Op(InstructionIn[31:26]),    // Use the opcode from the instruction
+        .Funct(InstructionIn[5:0]),   // Use the function code for R-type instructions
+        .MemWrite(MemWriteEnWire),    // Memory Write enable signal
+        .MemRead(MemReadEnWire),      // Memory Read enable signal
+        .RegWrite(RegWriteWire),      // Register Write enable signal
+        .RegRead(RegReadWire),        // Register Read (may reuse RegWrite)
+        .Memmode(MemMode)             // Memory mode (word, half, byte)
+    );
+
+    RegisterFile reg_file (
+        .ReadRegister1(InstructionIn[25:21]),   // Rs
+        .ReadRegister2(InstructionIn[20:16]),   // Rt
+        .WriteRegister(WBDestRdRtWire),         // Destination register (Rd/Rt)
+        .WriteData(DataWriteValWire),           // Data to be written to the register
+        .RegWrite(RegWriteWire),                // Control signal to enable writing
+        .Clk(clk),                              // Clock signal
+        .ReadData1(DataIn25_21Wire),                // Read data from Rs
+        .ReadData2(DataIn20_15Wire)                 // Read data from Rt
+    );
+
+
 endmodule
