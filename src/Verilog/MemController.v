33edf3f74073ef717819fad1422a109c5c0489b8
Ahmad Qureshi <ahmadsqureshi@arizona.edu>
2024-10-15 16:14:46 -0700

    Syntax fixes for synth

--- src/Verilog/MemController.v
+++ src/Verilog/MemController.v
@@ -1,20 +1,20 @@
 module MemController(
-    Op;
-    Funct;
-    MemWrite;
-    MemRead;
-    RegWrite;
-    RegRead;
-    Memmode;
+    Op,
+    Funct,
+    MemWrite,
+    MemRead,
+    RegWrite,
+    RegRead,
+    Memmode
 );
 
     input  [5:0] Op;
     input  [5:0] Funct;
-    output MemWrite;
-    output MemRead;
-    output RegWrite;
-    output RegRead;
-    output [1:0] Memmode;
+    output reg MemWrite;
+    output reg MemRead;
+    output reg RegWrite;
+    output reg RegRead;
+    output reg [1:0] Memmode;
     localparam X = 2'b0;
     localparam WORD = 2'b0;
     localparam HALF = 2'b1;
@@ -22,70 +22,70 @@ module MemController(
 
     always @(*) begin
         case (Op)
-            6b'100011 : begin   //Load Word
+            6'b100011 : begin   //Load Word
                MemWrite <= 0;
                MemRead <= 1;
                RegWrite <= 1;
                RegRead <= 1;
                Memmode <= WORD;
             end
-            6b'100001 : begin   //Load Half
+            6'b100001 : begin   //Load Half
                MemWrite <= 0;
                MemRead <= 1;
                RegWrite <= 1;
                RegRead <= 1;
                Memmode <= HALF;
             end
-            6b'100000 : begin   //Load Byte
+            6'b100000 : begin   //Load Byte
                MemWrite <= 0;
                MemRead <= 1;
                RegWrite <= 1;
                RegRead <= 1;
                Memmode <= BYTE;
             end
-            6b'101011 : begin   //Store Word
+            6'b101011 : begin   //Store Word
                MemWrite <= 1;
                MemRead <= 0;
                RegWrite <= 0;
                RegRead <= 1;
                Memmode <= WORD;
             end
-            6b'101001 : begin   //Store Half
+            6'b101001 : begin   //Store Half
                MemWrite <= 1;
                MemRead <= 0;
                RegWrite <= 0;
                RegRead <= 1;
                Memmode <= HALF;
             end
-            6b'101000 : begin   //Store Byte
+            6'b101000 : begin   //Store Byte
                MemWrite <= 1; 
                MemRead <= 0;
                RegWrite <= 0;
                RegRead <= 1;
                Memmode <= BYTE;
             end
-            6b'000101 : begin   //Branch not equal to
+            6'b000101 : begin   //Branch not equal to
                 MemWrite <= 0; 
                 MemRead <= 0;
                 RegWrite <= 0;
                 RegRead <= 1;
                 Memmode <= X;
              end
-             6b'000100 : begin   //Branch equal to
+             6'b000100 : begin   //Branch equal to
                 MemWrite <= 0; 
                 MemRead <= 0;
                 RegWrite <= 0;
                 RegRead <= 1;
                 Memmode <= X;
              end
-             6b'000010 : begin   //Jump
+             6'b000010 : begin   //Jump
                 MemWrite <= 0; 
                 MemRead <= 0;
                 RegWrite <= 0;
                 RegRead <= 0;
                 Memmode <= X;
              end
-             6b'000011 : begin   //Jump and Link
+             6'b000011 : begin   //Jump and Link
                 MemWrite <= 0; 
                 MemRead <= 0;
                 RegWrite <= 1;
