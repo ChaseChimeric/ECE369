cacf67e16eef434295815a675192de799248aa75
ChaseChimeric <ryfong704@gmail.com>
2024-11-04 18:41:21 -0800

    revert to 73a9b857ec5efc515c3047e7efbfe8bd755c4afc

--- src/Verilog/Decode.v
+++ src/Verilog/Decode.v
@@ -39,7 +39,6 @@ module Decode (
     input [4:0] RegWriteAddr;
     output reg [31:0] InstructionOut, NextInstructionOut, DataIn25_21, DataIn20_15;
     wire [31:0] DataIn25_21Wire, DataIn20_15Wire;
-    output [1:0] nextInstrControlWire;
 
     input clk, RegWriteIn, rst;
     output reg [2:0] ForceInstr;
@@ -129,15 +128,6 @@ module Decode (
             MemMode <= MemmodeWire;
         end
     end
-
-    BranchController branchController_inst (
-        .DataIn20_15(DataIn20_15Wire),
-        .DataIn25_21(DataIn25_21Wire),
-        .Full32BitInstruction(InstructionIn),
-        .nextInstrControl(nextInstrControlWire),
-        .jumpAddress()
-    );
-
 
     MUXController mux_ctrl (
         .funct(InstructionIn[5:0]),
