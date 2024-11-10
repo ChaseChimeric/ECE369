75f501857d3da444b992bad85dd15f3ed56fab7d
RyanMcDermott1014 <papatheinfamousrhino@gmail.com>
2024-11-10 08:47:48 -0700

    Fixed syntax of testing and file

--- src/TestBenches/HazardDetectionUnit_tb.v
+++ src/TestBenches/HazardDetectionUnit_tb.v
@@ -5,19 +5,47 @@ module HazardDetectionUnit_tb;
 
   //Ports
   reg [31:0] InstructionInDecode;
-  reg [31:0] InstuctionInMemory;
-  reg [31:0] InstuctionInExecute;
+  reg [31:0] InstructionInMemory;
+  reg [31:0] InstructionInExecute;
   reg [31:0] InstructionInWB;
   wire stall;
 
   HazardDetectionUnit  HazardDetectionUnit_inst (
     .InstructionInDecode(InstructionInDecode),
-    .InstuctionInMemory(InstuctionInMemory),
-    .InstuctionInExecute(InstuctionInExecute),
+    .InstructionInMemory(InstructionInMemory),
+    .InstructionInExecute(InstructionInExecute),
     .InstructionInWB(InstructionInWB),
     .stall(stall)
   );
 
-//always #5  clk = ! clk ;
+  initial begin
+    #100;
+    //R-TYPE ADD
+     // Test scenario 1: R-type instruction (ADD)
+    InstructionInDecode={6'b0,5'b0, 5'b1,5'b11, 5'b0,6'b100000};
+    InstructionInExecute={6'b0,5'b11, 5'b1,5'b11, 5'b0,6'b100000};
+    InstructionInWB={32'b0};
+    InstructionInMemory={32'b0};
+
+    #100;
+    $display("STALL: %d\n",stall);
+
+    InstructionInExecute={32'b0};
+    InstructionInMemory={6'b0,5'b0, 5'b11,5'b11, 5'b0,6'b100000};
+    InstructionInWB={32'b0};
+
+    #100;
+    $display("STALL: %d\n",stall);
+    InstructionInExecute={32'b0};
+    InstructionInMemory={32'b0};
+    InstructionInWB={6'b0,5'b0, 5'b1,5'b11, 5'b0,6'b100000};
+
+    #100;
+    $display("STALL: %d\n",stall);
+    #100;
+
+    $finish;
+
+  end
 
 endmodule
--- src/Verilog/HazardDetectionUnit.v
+++ src/Verilog/HazardDetectionUnit.v
@@ -1,40 +1,44 @@
 `ifndef hazarddetectionunit
 `define hazarddetectionunit
 module HazardDetectionUnit (
-    input[31:0] InstructionInDecode;
-    input[31:0] InstuctionInMemory;
-    input[31:0] InstuctionInExecute;
-    input[31:0] InstructionInWB;
-    output stall;
+    input[31:0] InstructionInDecode,
+    input[31:0] InstructionInMemory,
+    input[31:0] InstructionInExecute,
+    input[31:0] InstructionInWB,
+    output reg stall
 );
 //Dependency if original if there is an R-Type in the pipeline
-if(InstructionInMemory[31:26]==0'b000000 || InstructionInWB[31:26]==0'b000000 || [31:26] InstuctionInExecute == 0'b000000 ){
-    if(InstructionInDecode[31:26]==0'b000000 && //Instruction is R- Type 
-     ((InstructionInDecode[25:21]||InstructionInDecode[20:16]) == //Checks dependency  on either instruction
-     (InstructionInMemory[15:11] || InstructionInWB[15:11] || InstructionInExecute[15:11] ) ) ){
-        stall = 1;
-     }
-     if((
-        InstructionInDecode[31:26]==0'b001000 || //ADDI
-        InstructionInDecode[31:26]==0'b001100 || //ANDI     
-        InstructionInDecode[31:26]==0'b001110 || //XORI     
-        InstructionInDecode[31:26]==0'b001101 || //ORI     
-        InstructionInDecode[31:26]==0'b001010 || //SLTI     
-        InstructionInDecode[31:26]==0'b100011 || //LW     
-        InstructionInDecode[31:26]==0'b100001 || //LH     
-        InstructionInDecode[31:26]==0'b100000 || //LB     
-     ) && InstructionInDecode[25:21] == //Value to be used with immediate is equal to possible dependency
-     (InstructionInMemory[15:11] || InstructionInWB[15:11] || InstructionInExecute[15:11])        ){
-        stall = 1;
-     }
-     if((
-        InstructionInDecode[31:26]==0'b101011 || //SW
-        InstructionInDecode[31:26]==0'b101001 || //SH     
-        InstructionInDecode[31:26]==0'b101000 || //SB
-     ) && InstructionInDecode[20:16] == 
-     (InstructionInMemory[15:11] || InstructionInExecute[15:11])  
-     )
-}
+always @ (*) begin
+    if(InstructionInMemory[31:26]== 6'b000000 || InstructionInWB[31:26]== 6'b000000 ||  InstructionInExecute[31:26] == 6'b000000 )
+    begin
+            if(InstructionInDecode[31:26]== 6'b000000 && //Instruction is R- Type 
+            ((InstructionInDecode[25:21]||InstructionInDecode[20:16]) == //Checks dependency  on either instruction
+            (InstructionInMemory[15:11] || InstructionInWB[15:11] || InstructionInExecute[15:11] ) ) )
+            begin
+                stall = 1;
+            end
+            if((
+                InstructionInDecode[31:26]==6'b001000 || //ADDI
+                InstructionInDecode[31:26]==6'b001100 || //ANDI     
+                InstructionInDecode[31:26]==6'b001110 || //XORI     
+                InstructionInDecode[31:26]==6'b001101 || //ORI     
+                InstructionInDecode[31:26]==6'b001010 || //SLTI     
+                InstructionInDecode[31:26]==6'b100011 || //LW     
+                InstructionInDecode[31:26]==6'b100001 || //LH     
+                InstructionInDecode[31:26]==6'b100000    //LB     
+            ) &&
+             ((InstructionInDecode[25:21] == InstructionInMemory[15:11]) ||//Dependency Check
+             (InstructionInDecode[25:21] == InstructionInWB[15:11]) ||//Dependency check
+             (InstructionInDecode[25:21] == InstructionInExecute[15:11])))//Dependency check
+              
+            begin
+                stall = 1;
+            end     
+    end
+else begin
+    stall = 0;
+end
+end
     
 endmodule
 `endif
