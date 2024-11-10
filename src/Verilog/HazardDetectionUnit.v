4c1a854714056735db256227a557f52f22807d28
RyanMcDermott1014 <papatheinfamousrhino@gmail.com>
2024-11-10 12:11:08 -0700

    Hazards finished for r types, jumps and branches in the mem,wb,ex stages

--- src/Verilog/HazardDetectionUnit.v
+++ src/Verilog/HazardDetectionUnit.v
@@ -11,18 +11,18 @@ module HazardDetectionUnit (
 always @ (*) begin
     if(InstructionInMemory[31:26]== 6'b000000 || InstructionInWB[31:26]== 6'b000000 ||  InstructionInExecute[31:26] == 6'b000000 )
     begin
-            if(InstructionInDecode[31:26]== 6'b000000 && //Instruction is R- Type 
-            ((InstructionInDecode[25:21] == InstructionInMemory[15:11]) ||//Dependency Check
-             (InstructionInDecode[25:21] == InstructionInWB[15:11]) ||//Dependency check
-             (InstructionInDecode[25:21] == InstructionInExecute[15:11]) ||//Dependency check
+            if(InstructionInDecode[31:26] == 6'b000000 && //Instruction is R- Type 
+            (( InstructionInDecode[25:21] == InstructionInMemory [15:11]) ||//Dependency Check
+             ( InstructionInDecode[25:21] == InstructionInWB     [15:11]) ||//Dependency check
+             ( InstructionInDecode[25:21] == InstructionInExecute[15:11]) ||//Dependency check
              ((InstructionInDecode[20:16] == InstructionInMemory[15:11]) ||//Dependency Check
-             (InstructionInDecode[20:16] == InstructionInWB[15:11]) ||//Dependency check
-             (InstructionInDecode[20:16] == InstructionInExecute[15:11]))))//Dependency check
+             ( InstructionInDecode[20:16] == InstructionInWB     [15:11]) ||//Dependency check
+             ( InstructionInDecode[20:16] == InstructionInExecute[15:11]))))//Dependency check
 
             begin
                 stall = 1;
             end
-            if((
+            else if((
                 InstructionInDecode[31:26]==6'b001000 || //ADDI
                 InstructionInDecode[31:26]==6'b001100 || //ANDI     
                 InstructionInDecode[31:26]==6'b001110 || //XORI     
@@ -32,18 +32,81 @@ always @ (*) begin
                 InstructionInDecode[31:26]==6'b100001 || //LH     
                 InstructionInDecode[31:26]==6'b100000    //LB     
             ) &&
-             ((InstructionInDecode[25:21] == InstructionInMemory[15:11]) ||//Dependency Check
-             (InstructionInDecode[25:21] == InstructionInWB[15:11]) ||//Dependency check
-             (InstructionInDecode[25:21] == InstructionInExecute[15:11])))//Dependency check
+             ((InstructionInDecode[25:21] == InstructionInMemory [15:11]) ||//Dependency Check
+             ( InstructionInDecode[25:21]  == InstructionInWB     [15:11]) ||//Dependency check
+             ( InstructionInDecode[25:21]  == InstructionInExecute[15:11])))//Dependency check
               
             begin
                 stall = 1;
-            end     
+            end 
+
+            else if((
+                InstructionInDecode[31:26]==6'b101011 || //SW
+                InstructionInDecode[31:26]==6'b101001 || //SH    
+                InstructionInDecode[31:26]==6'b101000  //SB
+            ) &&
+            ((InstructionInDecode[20:16] == InstructionInMemory[15:11]) ||//Dependency Check
+             (InstructionInDecode[20:16] == InstructionInExecute[15:11])))
+
+            begin
+                stall = 1;
+            end
+            else if (InstructionInMemory[5:0]== 6'b001000 || InstructionInExecute[5:0]== 6'b001000 || InstructionInWB[5:0]== 6'b001000  ) begin
+                stall = 1;
+            end
+            else 
+            begin
+                stall = 0;
+            end   
     end
+else if (InstructionInMemory[31:26] ==  6'b000001 ||
+         InstructionInWB[31:26] ==      6'b000001 ||  
+         InstructionInExecute[31:26] == 6'b000001 ||//BGEZ & BLTZ
+         InstructionInMemory[31:26] ==  6'b000100 ||
+         InstructionInWB[31:26] ==      6'b000100 ||  
+         InstructionInExecute[31:26] == 6'b000100 ||//BEQ
+         InstructionInMemory[31:26] ==  6'b000101 ||
+         InstructionInWB[31:26] ==      6'b000101 ||  
+         InstructionInExecute[31:26] == 6'b000101 ||//BNE
+         InstructionInMemory[31:26] ==  6'b000111 ||
+         InstructionInWB[31:26] ==      6'b000111 ||  
+         InstructionInExecute[31:26] == 6'b000111 ||//BGTZ
+         InstructionInMemory[31:26] ==  6'b000001 ||
+         InstructionInWB[31:26] ==      6'b000001 ||  
+         InstructionInExecute[31:26] == 6'b000001 ||//BLEZ
+         InstructionInMemory[31:26] ==  6'b000010 ||
+         InstructionInWB[31:26] ==      6'b000010 ||  
+         InstructionInExecute[31:26] == 6'b000010 ||//J
+         InstructionInMemory[31:26] ==  6'b000011 ||
+         InstructionInWB[31:26] ==      6'b000011 ||  
+         InstructionInExecute[31:26] == 6'b000011   //JAL
+         )
+ begin
+    stall = 1;
+end
+else if (InstructionInMemory[31:26] ==  6'b000001 ||
+         InstructionInWB[31:26] ==      6'b000001 ||  
+         InstructionInExecute[31:26] == 6'b000001 ||//BGEZ & BLTZ 
+         begin
+    
+end
+
+
+
+
+
+
+//THIS ONE IS THE END
 else begin
     stall = 0;
 end
 end
+
+
+
+
+
+
     
 endmodule
 `endif
