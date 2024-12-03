6cf641f47a0a4844f30416cb586d573791b96090
ChaseChimeric <ryfong704@gmail.com>
2024-12-03 10:53:51 -0700

    fixed mult path for generation

--- src/scripts/random_array_gen.py
+++ src/scripts/random_array_gen.py
@@ -306,7 +306,7 @@ return:
 
 """)
         else:
-            outputfile.write("j multiply3\nend_mult3:\n")
+            outputfile.write("addi $t3, $0, 0\nj multiply3\nend_mult3:\n")
             outputfile.write("""sll $t3, $t3, 2
     add $a2, $t3, $a1 
     jal     vbsme           # call function
